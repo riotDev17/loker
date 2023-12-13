8c34234c189481a6d0934942b6d1008152013860
Muhammad Ridho Adhari <110064566+Jnosvb@users.noreply.github.com>
2023-12-13 21:33:59 +0700

    Setup

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -3,6 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Beranda extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model("Loker_model");
+		// $this->load->model("Kategori_model");
+	}
 	public function index()
 	{
 		$this->load->view('pelamar/beranda_view');
